d0af00304cc2b5339ee6d07b30cc7d2464551a2c
Fernando Almendro Martí <fernandoalmendromarti@gmail.com>
2021-11-04 18:34:12 +0100

    Tileset
    Project with tileset well added

---
+++ KirbyGameDev/Output/Assets/maps/Kirby Tileset 32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Kirby Tileset 32x32" tilewidth="32" tileheight="32" spacing="4" margin="4" tilecount="360" columns="24">
+ <image source="Kirby Tileset 32x32.png" width="870" height="544"/>
+</tileset>
