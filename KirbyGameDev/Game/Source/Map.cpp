974eb6182921d4439c85138132070c2d69177fc4
Fernando Almendro Martí <fernandoalmendromarti@gmail.com>
2021-12-09 20:58:01 +0100

    pathfinding

--- KirbyGameDev/Game/Source/Map.cpp
+++ KirbyGameDev/Game/Source/Map.cpp
@@ -4,6 +4,7 @@
 #include "Textures.h"
 #include "Map.h"
 
+
 #include "Defs.h"
 #include "Log.h"
 
@@ -27,6 +28,168 @@ bool Map::Awake(pugi::xml_node& config)
     folder.Create(config.child("folder").child_value());
 
     return ret;
+}
+
+void Map::ResetPath()
+{
+	frontier.Clear();
+	visited.clear();
+	breadcrumbs.clear();
+	path.Clear();
+
+	frontier.Push(iPoint(19, 4), 0);
+	visited.add(iPoint(19, 4));
+	breadcrumbs.add(iPoint(19, 4));
+
+	memset(costSoFar, 0, sizeof(uint) * COST_MAP_SIZE * COST_MAP_SIZE);
+}
+
+void Map::DrawPath()
+{
+	iPoint point;
+
+	// Draw visited
+	ListItem<iPoint>* item = visited.start;
+
+	while (item)
+	{
+		point = item->data;
+		TileSet* tileset = GetTilesetFromTileId(26);
+
+		SDL_Rect rec = tileset->GetTileRect(26);
+		iPoint pos = MapToWorld(point.x, point.y);
+
+		app->render->DrawTexture(tileset->texture, pos.x, pos.y, &rec);
+
+		item = item->next;
+	}
+
+	// Draw frontier
+	for (uint i = 0; i < frontier.Count(); ++i)
+	{
+		point = *(frontier.Peek(i));
+		TileSet* tileset = GetTilesetFromTileId(25);
+
+		SDL_Rect rec = tileset->GetTileRect(25);
+		iPoint pos = MapToWorld(point.x, point.y);
+
+		app->render->DrawTexture(tileset->texture, pos.x, pos.y, &rec);
+	}
+
+	// Draw path
+	for (uint i = 0; i < path.Count(); ++i)
+	{
+		iPoint pos = MapToWorld(path[i].x, path[i].y);
+		app->render->DrawTexture(tileX, pos.x, pos.y);
+	}
+
+}
+
+
+int Map::MovementCost(int x, int y) const
+{
+	int ret = -1;
+
+	if ((x >= 0) && (x < mapData.width) && (y >= 0) && (y < mapData.height))
+	{
+		int id = mapData.maplayers.start->next->data->Get(x, y);
+
+		if (id == 26) ret = 20;
+		else ret = 1;
+	}
+
+	return ret;
+}
+
+
+bool Map::IsWalkable(int x, int y) const
+{
+	// L10: DONE 3: return true only if x and y are within map limits
+	// and the tile is walkable (tile id 0 in the navigation layer)
+
+	bool isWalkable = false;
+	if (x >= 0 && y >= 0 && x < mapData.width && y < mapData.height) {
+
+		//gets the second layer
+		MapLayer* layer = mapData.maplayers.start->next->data;
+		int tileId = layer->Get(x, y);
+		if (tileId != 26) isWalkable = true;
+	}
+
+	return isWalkable;
+}
+
+void Map::ComputePath(int x, int y)
+{
+	path.Clear();
+	iPoint goal = WorldToMap(x, y);
+
+	// L11: DONE 2: Follow the breadcrumps to goal back to the origin
+	// add each step into "path" dyn array (it will then draw automatically)
+
+	path.PushBack(goal);
+	int index = visited.find(goal);
+
+	while ((index >= 0) && (goal != breadcrumbs[index]))
+	{
+		goal = breadcrumbs[index];
+		path.PushBack(goal);
+		index = visited.find(goal);
+	}
+
+}
+
+void Map::ComputePathAStar(int x, int y)
+{
+	// L12a: Compute AStart pathfinding
+}
+
+void Map::PropagateBFS()
+{
+	// L10: DONE 1: If frontier queue contains elements
+	// pop the last one and calculate its 4 neighbors
+	iPoint curr;
+	if (frontier.Pop(curr))
+	{
+		// L10: DONE 2: For each neighbor, if not visited, add it
+		// to the frontier queue and visited list
+		iPoint neighbors[4];
+		neighbors[0].create(curr.x + 1, curr.y + 0);
+		neighbors[1].create(curr.x + 0, curr.y + 1);
+		neighbors[2].create(curr.x - 1, curr.y + 0);
+		neighbors[3].create(curr.x + 0, curr.y - 1);
+
+		for (uint i = 0; i < 4; ++i)
+		{
+			if (IsWalkable(neighbors[i].x, neighbors[i].y))
+			{
+				if (visited.find(neighbors[i]) == -1)
+				{
+					frontier.Push(neighbors[i], 0);
+					visited.add(neighbors[i]);
+
+					// L11: DONE 1: Record the direction to the previous node 
+					// with the new list "breadcrumps"
+					breadcrumbs.add(curr);
+				}
+			}
+		}
+	}
+}
+
+void Map::PropagateDijkstra()
+{
+	// L11: TODO 3: Taking BFS as a reference, implement the Dijkstra algorithm
+	// use the 2 dimensional array "costSoFar" to track the accumulated costs
+	// on each cell (is already reset to 0 automatically)
+
+}
+
+void Map::PropagateAStar(int heuristic)
+{
+	// L12a: TODO 2: Implement AStar algorythm
+	// Consider the different heuristics
+
 }
 
 // Draw the map (all requried layers)
@@ -70,6 +233,8 @@ void Map::Draw()
 
 }
 
+
+
 // L04: TODO 8: Create a method that translates x,y coordinates from map positions to world positions
 iPoint Map::MapToWorld(int x, int y) const
 {
@@ -78,6 +243,53 @@ iPoint Map::MapToWorld(int x, int y) const
 	ret.y = y * mapData.tileHeight;
 
 	return ret;
+}
+
+iPoint Map::WorldToMap(int x, int y) const
+{
+	iPoint ret(0, 0);
+
+	// L05: DONE 3: Add the case for isometric maps to WorldToMap
+	if (mapData.type == MAPTYPE_ORTHOGONAL)
+	{
+		ret.x = x / mapData.tileWidth;
+		ret.y = y / mapData.tileHeight;
+	}
+	else if (mapData.type == MAPTYPE_ISOMETRIC)
+	{
+
+		float halfWidth = mapData.tileWidth * 0.5f;
+		float halfHeight = mapData.tileHeight * 0.5f;
+		ret.x = int((x / halfWidth + y / halfHeight) / 2);
+		ret.y = int((y / halfHeight - (x / halfWidth)) / 2);
+	}
+	else
+	{
+		LOG("Unknown map type");
+		ret.x = x; ret.y = y;
+	}
+
+	return ret;
+}
+
+
+TileSet* Map::GetTilesetFromTileId(int id) const
+{
+	ListItem<TileSet*>* item = mapData.tilesets.start;
+	TileSet* set = item->data;
+
+	while (item)
+	{
+		if (id < item->data->firstgid)
+		{
+			set = item->prev->data;
+			break;
+		}
+		set = item->data;
+		item = item->next;
+	}
+
+	return set;
 }
 
 // Get relative Tile rectangle
--- KirbyGameDev/Game/Source/Map.h
+++ KirbyGameDev/Game/Source/Map.h
@@ -4,8 +4,13 @@
 #include "Module.h"
 #include "List.h"
 #include "Point.h"
+#include "PQueue.h"
+#include "Queue.h"
+#include "DynArray.h"
 
 #include "PugiXml\src\pugixml.hpp"
+
+#define COST_MAP_SIZE	100
 
 // L03: DONE 2: Create a struct to hold information for a TileSet
 // Ignore Terrain Types and Tile Types for now, but we want the image!
@@ -102,6 +107,26 @@ public:
 	// L04: DONE 8: Create a method that translates x,y coordinates from map positions to world positions
 	iPoint MapToWorld(int x, int y) const;
 
+	iPoint WorldToMap(int x, int y) const;
+
+	// L10: BFS Pathfinding methods
+	void ResetPath();
+	void DrawPath();
+	bool IsWalkable(int x, int y) const;
+
+	// L11: More pathfinding methods
+	int MovementCost(int x, int y) const;
+	void ComputePath(int x, int y);
+
+	// L12a: AStar pathfinding
+	void ComputePathAStar(int x, int y);
+
+	// Propagation methods
+	void PropagateBFS();
+	void PropagateDijkstra();
+	// L12a: AStar propagation
+	void PropagateAStar(int heuristic);
+
 private:
 
 	bool LoadMap(pugi::xml_node mapFile);
@@ -115,6 +140,8 @@ private:
 	bool LoadLayer(pugi::xml_node& node, MapLayer* layer);
 	bool LoadAllLayers(pugi::xml_node mapNode);
 
+	TileSet* GetTilesetFromTileId(int id) const;
+
 public:
 
     // L03: DONE 1: Add your struct for map info
@@ -124,6 +151,21 @@ private:
 
     SString folder;
     bool mapLoaded;
+
+	// L10: BFS Pathfinding variables
+	PQueue<iPoint> frontier;
+	List<iPoint> visited;
+
+	// L11: Additional variables
+	List<iPoint> breadcrumbs;
+	uint costSoFar[COST_MAP_SIZE][COST_MAP_SIZE];
+	DynArray<iPoint> path;
+
+	// L12: AStar (A*) variables
+	iPoint goalAStar;			// Store goal target tile
+	bool finishAStar = false;	// Detect when reached goal
+
+	SDL_Texture* tileX = nullptr;
 };
 
 #endif // __MAP_H__
